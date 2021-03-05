41326ea9ae02d682a66fc4f716fdef09a109011c
AnjanaSuraj <79307344+AnjanaSuraj@users.noreply.github.com>
2021-03-05 11:24:13 +0530

    disabling endpointrouting for using mvc

--- src/counter/src/counterService/Startup.cs
+++ src/counter/src/counterService/Startup.cs
@@ -29,7 +29,7 @@ namespace Microsoft.ServiceFabricMesh.Samples.Counter.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddSingleton<Counter>(CreateCounter());
         }
 
